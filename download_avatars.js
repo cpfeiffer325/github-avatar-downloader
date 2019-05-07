8b95a6c5cd774627b815e0264a1052fbaccecd1e
Christian <c_pfeiffer325@yahoo.ca>
2019-05-07 14:32:33 +0000

    created avatar folder and hard coded a get request to import an avatar jpeg which is now working

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var token = require('./secret.js').GITHUB_TOKEN;
+var fs = require('fs');
+var https = require('https');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -17,6 +19,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
     // console.log(repo);
 }
 
+function downloadImageByURL(url, filepath) {
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log(`
+      Response Status Code: ${response.statusCode}
+      Response Status Message: ${response.statusMessage}
+      Response Headers: ${response.headers['content-type']}
+      `);
+      console.log('Downloading image...');
+    })
+    .on('end', function  () {
+      console.log('Download complete.');
+    })
+    .pipe(fs.createWriteStream(filepath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   // console.log("Errors:", err);
   // console.log("Result:", result);
@@ -27,3 +48,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 });
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+downloadImageByURL('https://avatars0.githubusercontent.com/u/1615?v=4', 'avatars/new_person.jpg');
