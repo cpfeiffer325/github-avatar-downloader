59f832815e27b1e631edd50fcdd55a5cd7c5dbf2
Christian <c_pfeiffer325@yahoo.ca>
2019-05-07 04:03:37 +0000

    fixed js so it actually uses the callback function, it now prints the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -11,13 +11,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
 };
 
   request(options, function(err, res, body) {
+    body = JSON.parse(body);
     cb(err, body);
-  });
+    });
+    // console.log(repo);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+    result.forEach(function(repo) {
+      console.log(repo.avatar_url);
+  });
+
 });
 
 console.log('Welcome to the GitHub Avatar Downloader!');
